1f8c614ba81eefbfe9f307be8f96c415c149ffd3
CrimsonAmir <ytchanneltinos@gmail.com>
2025-04-11 21:26:54 +0330

    fix some issue

--- server_side/user/authentication.py
+++ server_side/user/authentication.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from fastapi import Request, Depends, Response
+
+from fastapi import Request, Depends, Response, APIRouter, HTTPException
 from fastapi.responses import RedirectResponse
 import schemas, crud
 from sqlalchemy.orm import Session
@@ -27,7 +27,8 @@ token_black_list = TokenBlackList()
 @router.post('/sign-up/', response_model=schemas.SignUpReturn)
 async def create_user(user: schemas.SignUpRequirement, response: Response, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_phone_number(db, user.phone_number)
-    if db_user: return {'status': 'NOK', 'msg': 'this email already exists!'}
+    if db_user:
+        raise HTTPException(status_code=400, detail="this email already exists!")
 
     create_user_db = crud.create_user(db, user)
 
@@ -64,15 +65,15 @@ async def login(response: Response, user: schemas.LogInRequirement = None, db: S
 
     db_user = crud.get_user_by_phone_number(db, user.phone_number)
     if not db_user:
-        return {'status': 'NOK', 'msg': 'phone number does not exist'}
+        raise HTTPException(status_code=400, detail='phone number does not exist')
 
     request_hashed_password = hash_password_md5(user.password)
     if request_hashed_password != db_user.hashed_password:
-        return {'status': 'NOK', 'msg': 'password is not correct'}
+        raise HTTPException(status_code=400, detail='password is not correct')
 
     user_data = {
-        "first_name": user.first_name,
-        "user_id": user.user_id
+        "first_name": db_user.first_name,
+        "user_id": db_user.user_id
     }
 
     access_token = create_access_token(data=user_data)
