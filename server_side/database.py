32769cbb718a8fa582aaf881207903546b36e84b
Amir <140071494+Crimson-Amir@users.noreply.github.com>
2025-04-11 18:33:17 +0330

    Delete server_side directory

--- server_side/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:amir1383amir@localhost:5432/noon_yar"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
