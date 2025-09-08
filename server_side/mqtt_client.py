2c6c1abf3ffd2b629d4fe032011eb7081146ee28
CrimsonAmir <ytchanneltinos@gmail.com>
2025-09-08 17:09:08 +0330

    fix bug

--- server_side/mqtt_client.py
+++ server_side/mqtt_client.py
@@ -1,7 +1,29 @@
 import json
+import asyncio
+from asyncio_mqtt import Client, MqttError
 
 MQTT_BAKERY_PREFIX = "bakery/{0}"
 MQTT_UPDATE_BREAD_TIME = f"{MQTT_BAKERY_PREFIX}/bread_time_update"
+
+
+async def mqtt_handler(app):
+    client: Client = app.state.mqtt_client
+
+    try:
+        # Listen for all messages
+        async with client.unfiltered_messages() as messages:
+            await client.subscribe("bakery/+/error")  # subscribe to all bakeries errors
+
+            async for message in messages:
+                topic = message.topic
+                payload = message.payload.decode()
+                print(f"[MQTT ERROR] {topic}: {payload}")
+
+    except MqttError as e:
+        print(f"[MQTT ERROR] Connection lost: {e}")
+        # Optional: reconnect logic
+        await asyncio.sleep(5)
+        await mqtt_handler(app)  #
 
 async def update_time_per_bread(request, bakery_id, new_config):
     key = MQTT_UPDATE_BREAD_TIME.format(bakery_id)
