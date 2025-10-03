72d7a7dc37b2e2a40c93178068e7bffd245882b5
CrimsonAmir <ytchanneltinos@gmail.com>
2025-10-03 14:34:12 +0330

    update-docker

--- application/database.py
+++ application/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from setting import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:amir1383amir@localhost:5432/noon_yar"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
